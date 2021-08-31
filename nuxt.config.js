b7bbc76f82bff9413d1cbbb7f8d0a43854cd6663
ivoamaro <ivo.amaro.97.p@gmail.com>
2021-08-31 20:10:38 +0100

    r

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
         content: 'Carina Leitão Portfólio',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/porfolio/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/portfolio/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
