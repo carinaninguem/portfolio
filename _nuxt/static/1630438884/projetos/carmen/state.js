132b9352926d52d5e1aeee73448d318d5878467b
ivoamaro <ivo.amaro.97.p@gmail.com>
2021-08-31 20:44:12 +0100

    Updates

--- _nuxt/static/1630438884/projetos/carmen/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1630438884",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojetos\u002Fcarmen",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
