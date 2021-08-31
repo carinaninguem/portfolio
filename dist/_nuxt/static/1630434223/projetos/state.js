f8a2608ecd4ecaf76c2472200438f18fc8595773
ivoamaro <ivo.amaro.97.p@gmail.com>
2021-08-31 19:27:12 +0100

    r

--- dist/_nuxt/static/1630434223/projetos/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002Fdist\u002F_nuxt\u002Fstatic\u002F1630434223",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojetos",config:{_app:{basePath:"\u002Fportfolio\u002Fdist\u002F",assetsPath:"\u002Fportfolio\u002Fdist\u002F_nuxt\u002F",cdnURL:a}}}}(null));
